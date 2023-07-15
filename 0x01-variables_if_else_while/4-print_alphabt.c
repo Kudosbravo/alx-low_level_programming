33793efd28a088fd85cdc8b300ecd0f822638d51
Kudosbravo <okpojaanthony2@gmail.com>
2023-07-14 19:25:40 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,15 @@ int main(void)
 
 	while (n <= 122)
 	{
-		if (n == 101 || n == 113)
+		if (n == 101 | n == 113)
 		{
 			n++;
 			continue;
+		}
 		putchar(n);
 		n++;
 	}
 	putchar('\n');
+	return (0);
+}
 
